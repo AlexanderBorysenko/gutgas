b1460e0d226b33d3fce231e253a8eb980ef9c654
Alex <Alex@MacBook-Air.local>
2023-09-26 19:55:54 +0300

    Order Email

--- resources/views/emails/newOrder.blade.php
+++ resources/views/emails/newOrder.blade.php
@@ -7,72 +7,55 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<style>
-    table {
-        width: 100%;
-        border: 1px solid black;
-        border-collapse: collapse;
-    }
-
-    td,
-    th {
-        border: 1px solid black;
-        padding: 10px;
-    }
-</style>
 
 <body>
-    <table style="">
+    <table style="width: 100%;
+    border: 1px solid black;
+    border-collapse: collapse;">
         <tr>
-            <td style="">Імʼя</td>
-            <td>{{ $client_name }}</td>
+            <td style="border: 1px solid black;padding: 10px;">Імʼя</td>
+            <td style="border: 1px solid black;padding: 10px;">{{ $client_name }}</td>
         </tr>
 
         <tr>
-            <td>Пошта</td>
-            <td>{{ $client_email }}</td>
+            <td style="border: 1px solid black;padding: 10px;">Пошта</td>
+            <td style="border: 1px solid black;padding: 10px;">{{ $client_email }}</td>
         </tr>
 
         <tr>
-            <td>Телефон</td>
-            <td>{{ $client_phone }}</td>
+            <td style="border: 1px solid black;padding: 10px;">Телефон</td>
+            <td style="border: 1px solid black;padding: 10px;">{{ $client_phone }}</td>
         </tr>
 
         @isset($client_message)
             <tr>
-                <td>Повідомлення</td>
-                <td>{{ $client_message }}</td>
+                <td style="border: 1px solid black;padding: 10px;">Повідомлення</td>
+                <td style="border: 1px solid black;padding: 10px;">{{ $client_message }}</td>
             </tr>
         @endisset
     </table>
 
-    <table>
-        <thead>
+    <table style="width: 100%;
+    border: 1px solid black;
+    border-collapse: collapse;">
+        <thead style="border-bottom: 3px solid black;">
             <tr>
-                <th>id</th>
-                <th>Зображення</th>
-                <th>Назва</th>
-                <th>SKU</th>
-                <th>Кількість</th>
-                <th>Загалом</th>
+                <th style="border: 1px solid black;padding: 10px;">id</th>
+                <th style="border: 1px solid black;padding: 10px;">Назва</th>
+                <th style="border: 1px solid black;padding: 10px;">SKU</th>
+                <th style="border: 1px solid black;padding: 10px;">Кількість</th>
+                <th style="border: 1px solid black;padding: 10px;">Загалом</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($cart_content as $cart_item)
                 <tr>
-                    <td>{{ $cart_item['id'] }}</td>
-                    <td>
-                        <img src="{{ url('/') . $cart_item['media_file']['thumbnail']['url'] }}"
-                            style="
-                            width: 50px;
-                            height: 50px;
-                            object-fit: cover;
-                        " />
-                    </td>
-                    <td>{{ $cart_item['name']['ua'] }}</td>
-                    <td>{{ $cart_item['sku'] }}</td>
-                    <td>{{ $cart_item['quantity'] }}</td>
-                    <td>{{ $cart_item['price'] * $cart_item['quantity'] }} ₴</td>
+                    <td style="border: 1px solid black;padding: 10px;">{{ $cart_item['id'] }}</td>
+                    <td style="border: 1px solid black;padding: 10px;">{{ $cart_item['name']['ua'] }}</td>
+                    <td style="border: 1px solid black;padding: 10px;">{{ $cart_item['sku'] }}</td>
+                    <td style="border: 1px solid black;padding: 10px;">{{ $cart_item['quantity'] }}</td>
+                    <td style="border: 1px solid black;padding: 10px;">
+                        {{ $cart_item['price'] * $cart_item['quantity'] }} ₴</td>
                 </tr>
         </tbody>
         @endforeach
