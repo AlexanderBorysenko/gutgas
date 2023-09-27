c59ce41cd5f2e2b09700c8a5d994a40ea0c10835
Alex <Alex@MacBook-Air.local>
2023-09-27 21:18:10 +0300

    app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,11 @@
     {{-- <title inertia>{{ config('app.name', 'Laravel') }}</title> --}}
     <meta name="google-site-verification" content="Ul9Uz2ZUtv3wLZlr4YFWO_jKwt8MtdyluHt1vQimpqI">
 
+    <meta name="robots" content="noindex">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+
     @if (Request::is('admin*'))
         @php($__inertiaSsrDispatched = true)
         @php($__inertiaSsrResponse = null)
