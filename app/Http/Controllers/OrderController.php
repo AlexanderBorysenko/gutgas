dce1de642b977e8150ecb13506d537684ab9ca0a
Alex <Alex@MacBook-Air.local>
2023-12-22 19:50:57 +0200

    test

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -52,15 +52,41 @@ class OrderController extends Controller
         try {
             $order = Order::create($data);
 
-
             $data['order_id'] = $order->id;
+
+            // form message from following template
+            // 'client_name' => 'required|string|max:255',
+            // 'client_phone' => 'required|string|max:255',
+            // 'client_email' => 'required|string|regex:/(.+)@(.+)\.(.+)/i|max:255',
+            // 'client_message' => 'nullable|string|max:255',
+            // 'cart_content' => 'required|array',
+            // 'shipping_message' => 'nullable|string|max:255',
+            // 'status_name' => 'nullable|string|max:255',
+            // 'status_color' => 'nullable|string|max:255',
+            // 'compleated' => 'nullable|boolean',
+            $messageText = "Нове замовлення №{$order->id} від {$order->client_name} на суму {$order->total_price} грн.
+            Телефон: {$order->client_phone} 
+            Email: {$order->client_email} 
+            Коментар: {$order->client_message}  
+            Доставка: {$order->shipping_message}
+            Додані товари: ";
+            foreach ($order->cart_content as $product) {
+                $messageText .= "\n{$product['name']['ua']} - {$product['quantity']} шт. - {$product['price']}грн/шт.";
+            }
             Mail::send('emails.newOrder', $data, function ($message) {
                 $message->from('form-manager@gutgas.eu', 'Gutgas Sale manager');
-                $message->to('sale@gutgas.eu');
+                // $message->to('sale@gutgas.eu');
+                $message->to('borysenko.alexander@gmail.com');
                 $message->subject('$$$ Нове Замовлення $$$');
             });
 
             DB::commit();
+
+            $bot_token = '6483041228:AAE77cZN7t_Fd-5_Bnz1kC_1NWj9MBhiNFo';
+            $chat_id = '-4078811387';
+            $text = urlencode($messageText);
+            $url = "https://api.telegram.org/bot{$bot_token}/sendMessage?chat_id={$chat_id}&text={$text}";
+            file_get_contents($url);
 
             return redirect()->route('thankYou')
                 ->with('order', $order)->with('thankYouTranslations', trans('thank-you'));
