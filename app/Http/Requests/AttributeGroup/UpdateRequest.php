f2077ddb5a552df6a716f603c33a458ab1cb11e6
Alex <Alex@MacBook-Air.local>
2023-12-24 23:50:06 +0200

    update

--- app/Http/Requests/AttributeGroup/UpdateRequest.php
+++ app/Http/Requests/AttributeGroup/UpdateRequest.php
@@ -23,7 +23,6 @@ class UpdateRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'use_in_filters' => ['required', 'boolean'],
         ];
     }
 }
